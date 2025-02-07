42a73998b30aa1f7d96a3c61fb069a7f3eeeeb4c
Barrett Ray <66580279+onkoe@users.noreply.github.com>
2025-02-07 11:04:59 -0600

    fix(build.rs): also make `pyo3` build fix optional

--- build.rs
+++ build.rs
@@ -1,3 +1,7 @@
+#[cfg(not(feature = "python"))]
+fn main() {}
+
+#[cfg(feature = "python")]
 fn main() {
     // this fixes the weird macOS linking
     pyo3_build_config::add_extension_module_link_args();
